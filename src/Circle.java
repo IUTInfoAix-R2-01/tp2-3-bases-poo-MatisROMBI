f3b554227117db08b1e487db30491eca7911ce85
rombiMatis <matis.rombi@etu.univ-amu.fr>
2025-01-28 15:57:44 +0100

    ajout d'un nouveau constructeur "Circle" et d'un getColor

--- src/Circle.java
+++ src/Circle.java
@@ -13,11 +13,20 @@ public class Circle {
 		color = "red";
 	}
 	
+	public Circle(double r, String c) {
+		radius = r;
+		color = c;
+	}
+	
 	public double getRadius() {
 		return radius;
 	}
 	
 	public double getArea() {
 		return radius*radius*Math.PI;
+	}
+	
+	public String getColor() {
+		return color; 
 	}
 }
